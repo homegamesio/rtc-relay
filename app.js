7b05c81bd832390c7d912c7c5c63a5e4a8bf9011
Joseph Garcia <prosif@gmail.com>
2020-04-01 21:49:51 -0700

    its a class now

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
-const disableIceTrickle = desc => desc.replace(/\r\na=ice-options:trickle/g, '');
+class RTCRelay {
+    constructor(socketUrl, onOpen, onMessage, binaryType) {
+        this.connections = {};
+        this.channels = [];
+        this.onOpen = onOpen;
+        this.onMessage = onMessage;
+        this.socket = new WebSocket(socketUrl);
+        this.binaryType = binaryType;
 
-const rtcRelay = (socketUrl) => {
-    const connections = {};
-    const socket = new WebSocket(socketUrl);
+        if (this.binaryType) {
+            this.socket.binaryType = this.binaryType;
+        }
 
-    const becomeHost = () => new Promise((resolve, reject) => {
-        socket.onmessage = (msg) => {
-            const data = JSON.parse(msg.data);
-            if (data.type === 'HostResponse') {
-                resolve(data.success);
+        this.socket.onopen = async () => {
+            const isHost = await this.becomeHost();
+            if (isHost) {
+                this.listenForConnections();
+                this.socket.send('ready');
+                this.onOpen && this.onOpen();
             } else {
-                reject();
+                this.makePeerRequest().then(success => {
+                    if (!success) {
+                        this.socket.send('ready');
+                    }
+                    this.onOpen && this.onOpen();
+                });
             }
         };
+    }
+    
+    disableIceTrickle(desc) {
+        return desc.replace(/\r\na=ice-options:trickle/g, '');
+    }
 
-        socket.send(JSON.stringify({
-            type: "HostRequest"
-        }));
-    });
+    becomeHost() { 
+        return new Promise((resolve, reject) => {
+            this.socket.onmessage = (msg) => {
+                const data = JSON.parse(msg.data);
+                if (data.type === 'HostResponse') {
+                    resolve(data.success);
+                } else {
+                    reject();
+                }
+            };
 
-    const listenForConnections = () => {
-        const channels = [];
-        socket.onmessage = (msg) => {
+            this.socket.send(JSON.stringify({
+                type: "HostRequest"
+            }));
+        });
+    }
+
+    listenForConnections() {
+        this.socket.onmessage = (msg) => {
             if (typeof(msg.data) === 'string' && msg.data.charAt(0) === '{') {
                 const data = JSON.parse(msg.data);
                 if (data.type === 'PeerRequest') {
@@ -33,93 +62,86 @@ const rtcRelay = (socketUrl) => {
                             targetId: data.id,
                             offer: thing.localDescription
                         };
-                        socket.send(JSON.stringify(offerMessage));
+                        this.socket.send(JSON.stringify(offerMessage));
                     };
             
                     const dataChannel = thing.createDataChannel('homegames');
+
                     dataChannel.onopen = () => {
-                        channels.push(dataChannel);
+                        this.channels.push(dataChannel);
                     }
         
-                    connections[data.id] = thing;
+                    this.connections[data.id] = thing;
         
                     thing.createOffer().then((offer) => {
-                        const replacedSDP = disableIceTrickle(offer.sdp);
+                        const replacedSDP = this.disableIceTrickle(offer.sdp);
                         offer.sdp = replacedSDP;
     
                         thing.setLocalDescription(offer);
                     });
         
                 } else if (data.type === 'answer') {
-                    const connection = connections[data.targetId];
+                    const connection = this.connections[data.targetId];
     
                     if (connection.signalingState !== 'stable') {
                         connection.setRemoteDescription(new RTCSessionDescription(data.answer));
                     }
                 } else {
-                    this.onmessage && this.onmessage(msg.data);
+                    this.onMessage && this.onMessage(msg.data);
                 }
             } else {
-                for (let channelIndex in channels) {
-                    channels[channelIndex].send(msg.data)
+                for (let channelIndex in this.channels) {
+                    this.channels[channelIndex].send(msg.data)
                 }
-                this.onmessage && this.onmessage(msg.data);
+                this.onMessage && this.onMessage(msg.data);
             }
         };
-    };
+    }
 
-    const makePeerRequest = () => new Promise((resolve, reject) => {
-        socket.onmessage = (msg) => {
-            const data = JSON.parse(msg.data);
-            if (data.type === 'offer') {
-                const connection = new RTCPeerConnection({});
-     
-                connection.onicecandidate = (e) => {
-                    socket.send(JSON.stringify(connection.localDescription));
-                };
-
-                connection.ondatachannel = (e) => {
-                    const chan = e.channel || e;
-                    chan.onmessage = (msg) => this.onmessage(msg.data);
-                    resolve(true);
-                };
+    makePeerRequest() {
+        return new Promise((resolve, reject) => {
+            this.socket.onmessage = (msg) => {
+                const data = JSON.parse(msg.data);
+                if (data.type === 'offer') {
+                    const connection = new RTCPeerConnection({});
+         
+                    connection.onicecandidate = (e) => {
+                        this.socket.send(JSON.stringify(connection.localDescription));
+                    };
     
-                connection.setRemoteDescription(new RTCSessionDescription(data));
-                connection.createAnswer().then((answer) => {
-                    const replacedSDP = disableIceTrickle(answer.sdp);
-                    answer.sdp = replacedSDP;
-
-                    connection.setLocalDescription(answer);
-                });
-            }
-        };
+                    connection.ondatachannel = (e) => {
+                        const chan = e.channel || e;
     
-        socket.send(JSON.stringify({
-            type: "PeerRequest"
-        }));
-    });
+                        if (this.binaryType) {
+                            chan.binaryType = this.binaryType;
+                        }
 
-
-    socket.onopen = async () => {
-        const isHost = await becomeHost();
-        if (isHost) {
-            listenForConnections();
-            socket.send('ready');
-            this.onopen();
-        } else {
-            makePeerRequest().then(success => {
-                if (success) {
-                    console.log("I OPENED THAT BOI");
-                } else {
-                    socket.send('ready');
+                        chan.onmessage = (msg) => {
+                            this.onMessage && this.onMessage(msg.data);
+                        };
+                        resolve(true);
+                    };
+        
+                    connection.setRemoteDescription(new RTCSessionDescription(data));
+                    connection.createAnswer().then((answer) => {
+                        const replacedSDP = this.disableIceTrickle(answer.sdp);
+                        answer.sdp = replacedSDP;
+    
+                        connection.setLocalDescription(answer);
+                    });
                 }
-                this.onopen();
-            });
-        }
-    };
+            };
+        
+            this.socket.send(JSON.stringify({
+                type: "PeerRequest"
+            }));
+        });
+    }
 
-    return this;
-};
+    send(msg) {
+        this.socket.send(msg);
+    }
 
-module.exports = rtcRelay;
+}
 
+module.exports = RTCRelay;
